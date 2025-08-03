4996bd8b36b11dd4fef79b9796dd69d1ebbefdd4
NichFos <nicholasdfoster645@gmail.com>
2025-08-03 14:20:07 -0500

    test

--- terraform/deploy/backend.tf
+++ terraform/deploy/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "gcs" {
     bucket      = "terraformbucket61025"
-    prefix      = "terraform/state"
+    prefix      = "terraform/deploy"
     credentials = "class65gcpproject-462600-3dd7a46c5330.json"
   }
 
