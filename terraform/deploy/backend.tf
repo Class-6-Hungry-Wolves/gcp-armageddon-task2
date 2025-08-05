23047ad64e83bc1a02a2e0dac3c5076cd265e291
NichFos <nicholasdfoster645@gmail.com>
2025-08-04 21:38:05 -0500

    Trigger

--- terraform/deploy/backend.tf
+++ terraform/deploy/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "gcs" {
     bucket      = "terraformbucket61025"
     prefix      = "terraform/deploy"
-    credentials = "class65gcpproject-462600-3dd7a46c5330.json"
+    # credentials = "class65gcpproject-462600-3dd7a46c5330.json"
   }
 
   required_providers {
